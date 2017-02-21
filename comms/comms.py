a3ed0153c52f6d9c7a19ef3c93e1263283cd09f9
sgillen <sgillen@umd.edu>
2017-02-21 11:14:20 -0800

    tracking balls works now!

--- comms/comms.py
+++ comms/comms.py
@@ -7,16 +7,20 @@
 
 
 #In all cases the arduino will return B if it could not parse the input
+
 import serial
 import time
 
+#these are for the CV side of things. 
+from collections import deque
+import numpy as np
+import argparse
+import imutils
+import cv2
 
 
 
-ser = serial.Serial('/dev/tty.usbmodem1421',9600, timeout=3)
-time.sleep(3)
-print "serial port established (probably)"
-
+ 
 def get_pings():
     while(True):
         ser.write("s!")
@@ -46,18 +50,105 @@ def set_speed(left, right):
             print 'P received'
             return
         
+def find_ball():
+    	# grab the current frame
+	(grabbed, frame) = camera.read()
+
+    	# resize the frame, blur it, and convert it to the HSV
+	# color space
+	frame = imutils.resize(frame, width=600)
+
+        #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        #sg: if we want to add a blur use this, guassian is probably not a good idea
+        #blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+       	#hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
 
-#set_speed(100,100)
-#set_speed(100,200)
+        blurred = cv2.bilateralFilter(frame,9,75,75)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        
+	# construct a mask for the color, then perform
+	# a series of dilations and erosions to remove any small
+	# blobs left in the mask
+	mask = cv2.inRange(hsv, colorLower, colorUpper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+
+	# find contours in the mask and initialize the current
+	# (x, y) center of the ball
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
+
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+        
+                c = max(cnts, key=cv2.contourArea)
+                # print len(cnts)
+                #for c in cnts:
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+	        M = cv2.moments(c)
+	        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                
+                print center
+                
+	        height, width, channels = frame.shape
+                print "h = " + str(height)
+                print "c = " + str(center[0]/2) 
+                return (width/2 - center[0])
+
+                
 
 
 
-#TODO make sure the arduino is listening! 
+
+# define the lower and upper boundaries of the needed color
+# ball in the HSV color space, then initialize the
+# list of tracked points Current color:GREEN
+colorLower = (21, 80, 21)
+colorUpper = (110, 200, 200)
+pts =  deque(maxlen=64)
+
+#define some parameters for talking to the arduino/turning or whatever. 
+offset_thresh = 70
+
+# if a video path was not supplied, grab the reference
+# to the webcam
+
+camera = cv2.VideoCapture(0)
+        #cv2.waitKey(50)
+if not camera.isOpened():
+    print "something went wrong! video not open"
+
+
+# ser = serial.Serial('/dev/tty.usbmodem1421',9600, timeout=3)
+# time.sleep(3)
+# print "serial port established (probably)"
+
 
 
 while(True):
-    set_speed(100,100)
+    
+    offset = find_ball()
+    #can't find any ball, spin in circles
+    if(not offset):
+        #set_speed(-100, 100)
+        print "couldn't fine any ball"
+    elif(offset < offset_thresh and offset > -offset_thresh):
+        #set_speed(100, 100)
+        print "moving forward"
+    #ball is to the left
+    elif(offset < offset_thresh):
+        #set_speed(-100,100)
+        print "moving left"
+    #ball is to the right
+    elif(offset > offset_thresh):
+        #set_speed(100,-100)
+        print "moving right"
     #pings = get_pings()
     
 
